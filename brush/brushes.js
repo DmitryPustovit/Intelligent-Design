4c0f1763425a00deff85b92211b0c65402e90c84
bbeckwi2 <bbeckwi2@uncc.edu>
2018-04-05 16:30:41 -0400

    Added brushes  ༼ つ ◕_◕ ༽つ

---
+++ brush/brushes.js
@@ -0,0 +1,100 @@
+pencil = {
+	"name": "Pencil",
+	"textures": [
+		"pixel.png"
+	],
+	"mtRand": false,
+	"minRotation": 0,
+	"maxRotation": 0,
+	"step": 1,
+	"yOffset": 1,
+	"xOffset": 1,
+	"minScale": 1.0,
+	"maxScale": 1.0,
+	"minOpacity": 1.0,
+	"maxOpacity": 1.0,
+};
+
+pen = {
+	"name": "Pen",
+	"textures": [
+		"pen.png"
+	],
+	"mtRand": false,
+	"minRotation": 0,
+	"maxRotation": 0,
+	"step": 1,
+	"yOffset": 16,
+	"xOffset": 16,
+	"minScale": 0.5,
+	"maxScale": 0.5,
+	"minOpacity": 1.0,
+	"maxOpacity": 1.0,
+};
+
+horizontalBar = {
+	"name": "H-Bar",
+	"textures": [
+		"horizontal_bar.png"
+	],
+	"mtRand": false,
+	"minRotation": 0,
+	"maxRotation": 0,
+	"step": 1,
+	"yOffset": 16,
+	"xOffset": 16,
+	"minScale": 1.0,
+	"maxScale": 1.0,
+	"minOpacity": 1.0,
+	"maxOpacity": 1.0,
+};
+
+verticalBar = {
+	"name": "V-Bar",
+	"textures": [
+		"vertical_bar.png"
+	],
+	"mtRand": false,
+	"minRotation": 0,
+	"maxRotation": 0,
+	"step": 1,
+	"yOffset": 16,
+	"xOffset": 16,
+	"minScale": 1.0,
+	"maxScale": 1.0,
+	"minOpacity": 1.0,
+};
+
+bubbles = {
+	"name": "Bubbles",
+	"textures": [
+		"bubble.png", "bubble_small.png"
+	],
+	"mtRand": true,
+	"minRotation": 25,
+	"maxRotation": 180,
+	"step": 10,
+	"yOffset": 16,
+	"xOffset": 16,
+	"minScale": 0.5,
+	"maxScale": 1.0,
+	"minOpacity": 0.5,
+	"maxOpacity": 1.0,
+};
+
+clouds = {
+	"name": "Clouds",
+	"textures": [
+		"cloud.png"
+	],
+	"mtRand": false,
+	"minRotation": -90,
+	"maxRotation": 90,
+	"step": 3,
+	"yOffset": 16,
+	"xOffset": 16,
+	"minScale": 1.0,
+	"maxScale": 1.5,
+	"minOpacity": 0.1,
+	"maxOpacity": 0.5,
+};
