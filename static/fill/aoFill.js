3805a507007bb104c5225b9e80b2cb40438c8aa1
bbeckwi2 <bbeckwi2@uncc.edu>
2018-05-02 21:11:12 -0400

    Eh

--- static/fill/aoFill.js
+++ static/fill/aoFill.js
@@ -16,11 +16,14 @@
 
 function fillFromPoint(context, p1){
   var stack = [p1];
+  var width = context.canvas.clientWidth;
+  var height = context.canvas.clientHeight;
 
   while (stack.length > 0){
     var point, left, right, pos;
     point = stack.pop();
-    pos = (point.y )
-
+    pos = (point.y * width + x) * 4;
+    while (y-- > )
   }
 }
+
