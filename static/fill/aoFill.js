a03a0396d6b5cb3e8e64009136da189b558f5564
bbeckwi2 <bbeckwi2@uncc.edu>
2018-05-03 21:51:36 -0400

    The fill tool kinda works, but still is borkened

--- static/fill/aoFill.js
+++ static/fill/aoFill.js
@@ -22,15 +22,18 @@ var neigh = [[-1,0],[1,0],[0,-1],[0,1]];
 /* A fill function that fills from a point */
 function fillFromPoint(canvas, p1, rI, gI, bI, aI){
   var frontier = [];
-  var width = image.width;
-  var height = image.height;
-  var data = ctx.getImageData(0, 0, width, height);
+  var width = canvas.width;
+  var height = canvas.height;
+  var context = canvas.getContext('2d');
+  var imgData = context.getImageData(0, 0, width, height);
+  console.log(imgData.data);
   var pos = (p1.y * width + p1.x) * 4;
+  console.log(pos);
   frontier.push(p1);
-  var r = data[pos];
-  var g = data[pos + 1];
-  var b = data[pos + 2];
-  console.log(r); //This is the issue
+  var r = imgData.data[pos];
+  var g = imgData.data[pos + 1];
+  var b = imgData.data[pos + 2];
+  console.log(r + " " + g + " " + b); //This is the issue
 
   while (frontier.length){
     var point, left, right;
@@ -38,25 +41,24 @@ function fillFromPoint(canvas, p1, rI, gI, bI, aI){
     console.log("Removing");
     pos = (point.y * width + point.x) * 4;
 
-    if (data[pos] == r && data[pos + 1] == g && data[pos + 2] == b){
+    if (imgData.data[pos] == r && imgData.data[pos + 1] == g && imgData.data[pos + 2] == b){
       console.log("Updating!");
-      data[pos] = rI;
-      data[pos + 1] = gI;
-      data[pos + 2] = bI;
-      data[pos + 3] = aI;
+      imgData.data[pos] = rI;
+      imgData.data[pos + 1] = gI;
+      imgData.data[pos + 2] = bI;
+      imgData.data[pos + 3] = aI;
       for (var i=0; i < neigh.length; i++){
         var tPosX = point.x + neigh[i][0];
-        var tPosY = point.y + neigh[i][0];
+        var tPosY = point.y + neigh[i][1];
         if (tPosX >= 0 && tPosX < width && tPosY >= 0 && tPosY < height){
           var tPos = (tPosX * width + tPosY * 4);
-          if (data[tPos] == r && data[tPos + 1] == g && data[tPos + 2] == b){
-            console.log("Adding!");
+          if (imgData.data[tPos] == r && imgData.data[tPos + 1] == g && imgData.data[tPos + 2] == b){
             frontier.push(new Point(tPosX, tPosY));
           }
         }
       }
     }
   }
-  //return data;
-  ctx.putImageData(data,20,20);
+  //return imgData;
+  context.putImageData(imgData,0,0  );
 }
