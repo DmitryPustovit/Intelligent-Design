da746a6654a43f62da8f9ccf2d84bb2cecf925df
Dmitry <dmitryp222@gmail.com>
2018-05-04 01:11:53 -0400

    WORK

--- app.py
+++ app.py
@@ -45,6 +45,5 @@ def main():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    #socketio.run(app, host='0.0.0.0', port=os.environ.get("PORT", 5000))
-    socketio.run(app, host='0.0.0.0');
+    socketio.run(app, host='0.0.0.0', port=os.environ.get("PORT", 5000))
     #app.run(host='0.0.0.0', port=8080)
