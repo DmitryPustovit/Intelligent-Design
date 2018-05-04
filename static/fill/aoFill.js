960d16f823fd47d39da61d8cac811e71237ec6f2
Dmitry <dmitryp222@gmail.com>
2018-05-03 22:22:49 -0400

    test

--- static/fill/aoFill.js
+++ static/fill/aoFill.js
@@ -20,7 +20,7 @@ function Point(x, y){ this.x = x; this.y = y; return this; }
 var neigh = [[-1,0],[1,0],[0,-1],[0,1]];
 
 /* A fill function that fills from a point */
-function fillFromPoint(canvas, p1, rI, gI, bI){
+function fillFromPoint(p1, rI, gI, bI){
   var frontier = [];
   var width = canvas.width;
   var height = canvas.height;
